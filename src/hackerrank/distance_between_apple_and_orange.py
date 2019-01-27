3d34db25f11c47f88d29d57a01e83ea3243b2b43
Asya Isaeva <asya.gush@gmail.com>
2019-01-27 20:04:51 +0100

    Task: Apples and Oranges falling on Sam's house

---
+++ src/hackerrank/distance_between_apple_and_orange.py
@@ -0,0 +1,20 @@
+def count_apples_and_oranges(s, t, a, b, apples, oranges):
+    apple_in_home, orange_in_home = [0, 0]
+    home = list(range(s, t+1))
+    new_apples = [apples[i] + a for i in range(len(apples))]
+    new_oranges = [oranges[k] + b for k in range(len(oranges))]
+    for i in range(len(new_apples)):
+        if new_apples[i] in home: apple_in_home = apple_in_home+1
+    for k in range(len(new_oranges)):
+        if new_oranges[k] in home: orange_in_home = orange_in_home +1
+    print(f'{apple_in_home} \n{orange_in_home}')
+
+
+def test():
+        return count_apples_and_oranges(7, 11, 0, 15, [2, 5, 7], [5, -5])
+
+
+if __name__ == '__main__':
+        test()
+
+
