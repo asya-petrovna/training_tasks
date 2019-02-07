04938de4992e087df93bc3d6503a6315d91287e8
Asya Isaeva <asya.gush@gmail.com>
2019-02-07 22:50:06 +0100

    more test cases for Kangaroo

--- src/hackerrank/test/cangaroos_jumps_test.py
+++ src/hackerrank/test/cangaroos_jumps_test.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
+
 from parameterized import parameterized
+
 from .. import cangaroos_jumps
 
 
@@ -9,6 +11,11 @@ class TestKangaroo(TestCase):
         [(4, 7, 4, 0), True, 'Equal starting positions'],
         [(4, 7, 5, 7), False, 'Equal speeds'],
         [(-1, -9, -5, -7), True, 'Negative speeds, first kangaroo is on the right'],
+        [(-8, -3, -5, -7), False, 'Negative speeds, first kangaroo is on the left'],
+        [(0, 2, 14, 5), True, 'Moving towards each other'],
+        [(5000, 5000, 8000, 2000), True, 'Max values'],
+        [(500, 200, 2, 3), False, 'Big distance at start'],
+        [(0.01, 0.01, 0.11, 0.1), True, 'Small values'],
     ])
     def testParameterized(self, args, expected_result, name):
         actual_result = cangaroos_jumps.kangaroo(*args)
